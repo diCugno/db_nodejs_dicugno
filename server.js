edf4569dc1e0c13e7e660aa67cd9a5bce6586a67
diCugno <dialessandro@itis-molinari.eu>
2025-02-06 18:03:42 +0100

    modifica file

--- server.js
+++ server.js
@@ -1,43 +1,106 @@
 const express = require("express");
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
-const multer = require('multer');
-
 const app = express();
-
-const uploadDirectory = path.join(__dirname, 'files');
-
-let storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, uploadDirectory);
-    },
-    filename: function (req, file, callback) {
-        callback(null, file.originalname);
-    }
-});
-
-const upload = multer({ storage: storage }).single('file');
+const bodyParser = require('body-parser');
+const http = require("http");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+const path = require('path');
 app.use("/", express.static(path.join(__dirname, "public")));
-app.use("/files", express.static(path.join(__dirname, "files")));
 
-// aggiunta filelist
-app.get("/filelist", (req, res) => {
-    let files = fs.readdirSync(uploadDirectory);
-    files = files.map(file => "./files/" + file);
-    res.json(files);
-});
+const fs = require('fs');
+const mysql = require('mysql2');
+const conf = JSON.parse(fs.readFileSync("conf.json"));
+conf.ssl.ca = fs.readFileSync(__dirname + '/ca.pem');
+const connection = mysql.createConnection(conf);
 
 
-app.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-        console.log('File caricato:', req.file.filename);
-        res.json({ url: "./files/" + req.file.filename });
+
+const executeQuery = (sql) => {
+    return new Promise((resolve, reject) => {      
+          connection.query(sql, function (err, result) {
+             if (err) {
+                console.error(err);
+                reject();     
+             }   
+             console.log('done');
+             resolve(result);         
+       });
+    })
+ }
+
+ const createTable = () => {
+    return executeQuery(`
+    CREATE TABLE IF NOT EXISTS todo
+       ( id INT PRIMARY KEY AUTO_INCREMENT, 
+          name VARCHAR(255) NOT NULL, 
+          completed BOOLEAN ) 
+       `);      
+ }
+
+ const insert = (todo) => {
+    const template = `
+    INSERT INTO todo (name, completed) VALUES ('$NAME', '$COMPLETED')
+       `;
+    let sql = template.replace("$NAME", todo.name);
+    sql = sql.replace("$COMPLETED", todo.completed ? 1 : 0);
+    return executeQuery(sql); 
+ }
+
+ const select = () => {
+    const sql = `
+    SELECT id, name, completed FROM todo 
+       `;
+    return executeQuery(sql); 
+ }
+
+
+ createTable().then(() => {
+    insert({name: "test " + new Date().getTime(), completed: false}).then((result) => {
+       select().then(console.log);
     });
+ });
+
+
+
+
+let todos = [];
+
+app.post("/todo/add", (req, res) => {
+   const todo = req.body.todo;
+   todo.id = "" + new Date().getTime();
+   todos.push(todo);
+   res.json({result: "Ok"});
 });
 
-// Avvia il server
+app.get("/todo", (req, res) => {
+   res.json({todos: todos});
+});
+
+app.put("/todo/complete", (req, res) => {
+    const todo = req.body;
+    try {
+       todos = todos.map((element) => {
+          if (element.id === todo.id) {
+             element.completed = true;
+          }
+          return element;
+       })
+    } catch (e) {
+       console.log(e);
+    }
+    res.json({result: "Ok"});
+ });
+
+
+ app.delete("/todo/:id", (req, res) => {
+    todos = todos.filter((element) => element.id !== req.params.id);
+    res.json({result: "Ok"});  
+ })
+
 const server = http.createServer(app);
-server.listen(5600, () => {
-    console.log("- server running");
+
+server.listen(5500, () => {
+  console.log("- server running");
 });
